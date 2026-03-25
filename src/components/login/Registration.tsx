1164b8dc29e45e1d5b6bd342060c9e3040ab4e5a
samy470 <messaoudi.191931046501@gmail.com>
2026-03-25 20:55:18 +0100

    Update Registration.tsx

--- src/components/login/Registration.tsx
+++ src/components/login/Registration.tsx
@@ -41,7 +41,7 @@ const Register = () => {
         }
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:8080/api/register', {
+            const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
                 body: JSON.stringify(data),
